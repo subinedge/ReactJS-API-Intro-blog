e11dd5881d6fae320481ebc7343389dc73d17433
Subin <subinsamrat96@gmail.com>
2020-08-19 23:16:58 +0530

    react js api blog post completed

--- src/App.js
+++ src/App.js
@@ -1,26 +1,33 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import "./App.css";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  state = {
+    advice: "",
+  };
+
+  fetchQuote = () => {
+    axios.get("https://api.adviceslip.com/advice").then((response) => {
+      this.setState({ advice: response.data.slip.advice });
+    });
+  };
+
+  componentDidMount() {
+    this.fetchQuote();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1>Random Quote Generator</h1>
+        <button onClick={this.fetchQuote}>
+          <span>Fetch Quote</span>
+        </button>
+        <h3>{this.state.advice}</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
